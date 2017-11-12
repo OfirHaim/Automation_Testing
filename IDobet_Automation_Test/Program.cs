7398d584ac367e7603d3432b1460d31b7ff5e539
ofir <ofir@DESKTOP-7HQRMRL>
2017-11-12 14:19:33 +0200

    first commit install selenium and NUINT

---
+++ IDobet_Automation_Test/Program.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDobet_Automation_Test
+{
+    class Program
+    {
+        //crate a referance for our browser
+        IWebDriver driver = new ChromeDriver();
+
+        static void Main(string[] args)
+        {
+        }
+
+        //SetUp run Before any test
+        [SetUp]
+        public void Initialize()
+        {
+            //write to the console
+            Console.WriteLine("OpenUrl");
+
+            //neavigate to any url 
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=&amp;Password=&amp;Login=Login﻿");
+        }
+
+        //Test is the test case
+        [Test]
+        public void ExecuiteTest()
+        {
+            //Title
+            SeleniumSetMethod.SlectDropDown(driver, "TitleId", "Mr.", "Id");
+
+            //Intail
+            SeleniumSetMethod.EnterText(driver, "Initial", "executeautomation", "Name");
+
+            //Click cc
+            SeleniumSetMethod.Click(driver, "Save", "Name");
+        }
+
+        [Test]
+        public void NextTest()
+        {
+            Console.WriteLine("Next Method");
+        }
+
+        //TearDown run after any TEST
+        [TearDown]
+        public void CleanUp()
+        {
+            //write to the console
+            Console.WriteLine("CleanUp");
+
+            //close the driver
+            driver.Close();
+        }
+    }
+}
---
+++ IDobet_Automation_Test/SeleniumSetMethod.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace IDobet_Automation_Test
+{
+    class SeleniumSetMethod
+    {
+        //Enter Text
+        public static void EnterText(IWebDriver driver,string element, string value, string elementtype)
+        {
+            if(elementtype == "id")
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).SendKeys(value);
+        }
+
+        //Click into a button , Checkbox, Optin etc
+        public static void Click(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "id")
+                driver.FindElement(By.Id(element)).Click();
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).Click();
+        }
+
+        //Slecting a drop down control
+        public static void SlectDropDown(IWebDriver driver, string element, string value, string elementtype)
+        {
+            if (elementtype == "id")
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+
+            if (elementtype == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+        }
+    }
+}
